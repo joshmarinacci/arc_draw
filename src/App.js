9a543c4d9ba22da1ea1327eecf35bcfde2e45c7f
josh <joshua@marinacci.org>
2021-06-07 13:13:16 -0700

    added a color picker button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useEffect, useRef, useState} from 'react'
-import {SketchPicker} from "react-color"
+import {ChromePicker, SketchPicker} from "react-color"
 
 export const HBox = ({children}) => {
   return <div className={"hbox"}>{children}</div>
@@ -256,7 +256,8 @@ const BufferEditor = ({width, height, initialZoom}) => {
       <button onClick={()=>set_buffer(buffer.shift(0,-1))}>shift up</button>
       <button onClick={()=>set_buffer(buffer.shift(-1,0))}>shift left</button>
       <button onClick={()=>set_buffer(buffer.shift(1,0))}>shift right</button>
-      <SketchPicker color={buffer.fgcolor} onChange={(c)=> set_buffer(buffer.set_fg_color(c.hsl))}/>
+      <ColorPickerButton color={buffer.fgcolor} onChange={(c)=> set_buffer(buffer.set_fg_color(c.hsl))}/>
+      {/*<ColorPickerButton color={buffer.fgcolor}*/}
     </VBox>
     <canvas className={"drawing-surface"} ref={ref} width={width} height={height} onClick={handle_click}  />
   </HBox>
@@ -267,6 +268,55 @@ function App() {
   return <HBox>
     <BufferEditor width={1280} height={768} initialZoom={4}/>
   </HBox>
+}
+
+function hsl_to_css(color) {
+  return `hsl(${Math.floor(color.h)},${Math.floor(color.s*100)}%,${color.l*100}%)`
+}
+
+const ColorPickerButton = ({color,onChange})=>{
+  const [show, set_show] = useState(false)
+  const handle_click = () => {
+    set_show(!show)
+  }
+  const handle_close = () => {
+    set_show(false)
+  }
+
+  const popover = {
+    position: 'absolute',
+    zIndex: '2',
+  }
+  const cover = {
+    position: 'fixed',
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
+  }
+  const swatch = {
+        padding: '5px',
+        background: '#fff',
+        borderRadius: '1px',
+        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+        display: 'inline-block',
+        cursor: 'pointer',
+  }
+  const colorStyle = {
+      width: '36px',
+      height: '14px',
+      borderRadius: '2px',
+      background: hsl_to_css(color)
+  }
+
+
+  return <div>
+    <div style={swatch} onClick={()=>handle_click()}><div style={colorStyle}/></div>
+    { show ? <div style={ popover }>
+      <div style={cover} onClick={handle_close}/>
+      <ChromePicker color={color} onChange={onChange} />
+    </div> : null }
+  </div>
 }
 
 export default App;
